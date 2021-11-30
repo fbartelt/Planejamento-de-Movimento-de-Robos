d6b599f29861121a5abed0638e191af5543dca3f
fbartelt <fofispirocudo22@gmail.com>
2021-11-30 14:01:15 -0300

    Finished potential function algorithm

--- catkin_ws/src/potential_function/scripts/potential_function.py
+++ catkin_ws/src/potential_function/scripts/potential_function.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 import rospy
-import tf2_ros as tf2
 import sys
 import geometry_msgs.msg as gmsg
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import euler_from_quaternion
 import math
 import numpy as np
-import pandas as pd
 
 #User defined
 theta = 0.001 #Angle about z axis
 err = 0.3 #Position tolerance in meters
-wstar = 0.8 #Safety distance W*
 Kp = 1 #Controller proportional gain
 d = 0.8 #For feedback linearization
-Vmax = 5 #Max velocity for the robot
-zeta = 4
-d_star = 1
-eta = 5
-Q_star = 6
+Vmax = 20 #Max velocity for the robot
+zeta = 5
+d_star = 3
+eta = 300
+Q_star = 1.2
+alpha = 0.2
 
 #Laser params
 laser_range_min = 0
@@ -30,16 +28,14 @@ laser_ang_min = 0
 laser_ang_max = 0
 
 pos = gmsg.Point()
-pos.x = np.inf # guarantees no instant end of path planning
-pos.y = np.inf
+pos.x = 1000
+pos.y = 1000
 pub = None
 br = None
 ranges_ = []
 counter = 0
 state = 0
-d_reach = 0
-d_followed = 0
-states = {0 : 'Motion to Goal', 1 : 'Boundary Following', 2 : 'Reached Goal'}
+states = {0 : 'Motion to Goal', 2 : 'Reached Goal!!!'}
 
 def get_laser_params(data):
     """Sets global laser parameters."""
@@ -53,7 +49,6 @@ def get_laser_params(data):
 
 def angle2goal(x_goal, y_goal):
     """Computes the angle to reach the goal in respect to world coords.
-    Used to check if an obstacle is obstructing the straight path.
     """
     ang = math.atan2(y_goal - pos.y, x_goal - pos.x) - theta
     ang = (ang + np.pi) % (2 * np.pi) - np.pi # get angle in [-pi, pi)
@@ -77,15 +72,10 @@ def range2cart(ranges, idx):
     
     return range_world[:2]
 
-def find_continuities(ranges):
+def find_continuities():
     """ Returns a list of continuities intervals (min, max) for each
     detected obstacle, i.e. the closed intervals of <ranges> indices
     where an obstacle is detected (a continuity).
-
-    Params
-    ------
-    ranges: sensor_msgs.msg.LaserScan.ranges
-        The ranges returned by LaserScan messages.
     
     Returns
     -------
@@ -95,7 +85,7 @@ def find_continuities(ranges):
         where the condition is satisfied.
     """
     # Get indices where laser range < range_max
-    cont_indx = np.nonzero(np.array(ranges) < laser_range_max)[0]
+    cont_indx = np.nonzero(np.array(ranges_) < laser_range_max)[0]
     # Get superior and inferior limits of continuities
     lim_sup = np.array([x for i, x in enumerate(cont_indx) 
                         if (x + 1 != cont_indx[(i + 1) % len(cont_indx)])])
@@ -106,6 +96,24 @@ def find_continuities(ranges):
     return cont_lims
 
 def attractive_potential(q, q_goal):
+    """Implements a conic + quadratic attractive potential. When the
+    robot is more than <d_star> meters from the goal, the conic
+    potential is used, otherwise quadratic potential is used.
+
+    Params
+    ------
+    q: np.array
+        The robot's current configuration
+    q_goal: np.array
+        The goal configuration
+
+    Returns
+    -------
+    U_att: float
+        The attractive potential value
+    grad_Uatt: np.array
+        The attractive potential gradient
+    """
     d_qqgoal = np.linalg.norm(q - q_goal)
     if d_qqgoal <= d_star:
         U_att = 0.5 * zeta * d_qqgoal ** 2
@@ -117,19 +125,36 @@ def attractive_potential(q, q_goal):
     return U_att, grad_Uatt
 
 def repulsive_potential(q):
+    """Implements a repulsive potential as a sum of every repulsive
+    potential given by each obstacle in sight. If the robot is more
+    than <Q_star> meters from the obstacle, the potential function is
+    zero.
+
+    Params
+    ------
+    q: np.array
+        The robot's current configuration
+
+    Returns
+    -------
+    U_rep: float
+        The total repulsive potential value
+    grad_Urep: np.array
+        The total repulsive potential gradient
+    """
     U_rep = 0
     grad_Urep = 0
-    regions = find_continuities(ranges_)
+    regions = find_continuities()
 
     for obstacle in regions:
         obs_inf, obs_sub = obstacle
         range_idx_list = np.unique(np.linspace(obs_inf, obs_sub, dtype=np.int64))
-        oi_mat = get_oi_coord(range_idx_list)
-        d_c = np.linalg.norm((q - oi_mat), axis=1)
+        qoi_mat = get_oi_config(range_idx_list)
+        d_c = np.linalg.norm((q - qoi_mat), axis=1)
         idx_minc = np.argmin(d_c)
         d_qqobs = d_c[idx_minc]
-        c = oi_mat[idx_minc, :]
-        grad_d = (q - c) / d_c
+        c = qoi_mat[idx_minc, :]
+        grad_d = (q - c) / d_qqobs
         
         if d_qqobs <= Q_star:
             U_rep += 0.5 * eta * (1 / d_qqobs - 1 / Q_star)**2
@@ -140,28 +165,29 @@ def repulsive_potential(q):
     
     return U_rep, grad_Urep
 
-def get_oi_coord(cont_idx_list):
-    """Returns a matrix with world cartesian coords of each obstacle.
+def get_oi_config(ranges_idx_list):
+    """Returns a matrix with the configuration of each obstacle.
 
     Params
     ------
-    ranges: sensor_msgs.msg.LaserScan.ranges
-        The ranges returned by LaserScan messages.
-    cont_idx_list: list
+    ranges_idx_list: list
         A list of each <ranges> indices that contains a Oi (endpoint).
     
     Returns
     -------
-    oi_mat: np.array
-        A matrix that contains each endpoint Oi coords as a row vector,
-        i.e. [ [Oi_x1, Oi_x1], [Oi_x2, Oi_x2], ..., [Oi_xn, Oi_xn] ].
+    qoi_mat: np.array
+        A matrix that contains each endpoint Oi configuration as a row 
+        vector, i.e. 
+        [ [Oi1_x, Oi1_y, Oi1_theta], ..., [Oin_x, Oin_y, Oin_theta] ].
 
     """
-    oi_mat = np.empty((len(cont_idx_list), 2))
-    for i, x in enumerate(cont_idx_list):
-        oi_mat[i, :] = range2cart(ranges_, int(x))
+    qoi_mat = np.empty((len(ranges_idx_list), 3))
+    for i, x in enumerate(ranges_idx_list):
+        oi_coord = range2cart(ranges_, int(x))
+        oi_theta = angle2goal(oi_coord[0], oi_coord[1])
+        qoi_mat[i, :] = np.r_[oi_coord, oi_theta]
 
-    return oi_mat
+    return qoi_mat
     
 def traj_controller(x_goal, y_goal, vx=0, vy=0):
     global Kp, pos, Vmax, theta, d
@@ -203,44 +229,37 @@ def callback_pose(data):
 
 def change_state(n):
     global state
-    print(f'Stage changed from {state} to {n}')
     state = n
+    print(f'{states[state]}')
 
 def run(x_goal, y_goal):
-    global pub, br, counter
-    br = tf2.TransformBroadcaster()
-    t = gmsg.TransformStamped()
+    global pub, counter
     twist = gmsg.Twist()
-    rospy.init_node('tangent_bug', anonymous=True)
-    #t.header.stamp = rospy.Time.now()
-    #t.header.frame_id = "ZaWarudo"
-    #t.child_frame_id = 'robot_0'
+    rospy.init_node('potential_function', anonymous=True)
     pub = rospy.Publisher('robot_0/cmd_vel', gmsg.Twist, queue_size=1)
-    #goal_sub = rospy.Subscriber('robot_1/odom', Odometry, set_goal, (x_goal, y_goal))
-    #goal_sub.unregister()
     scan_sub = rospy.Subscriber('robot_0/base_scan', LaserScan, callback_scan)
     pos_sub = rospy.Subscriber('robot_0/base_pose_ground_truth', Odometry, callback_pose)
     rate = rospy.Rate(20)
+    U_grad = np.array([100, 100])
+
     while not rospy.is_shutdown():
-        if np.linalg.norm([pos.x - x_goal, pos.y - y_goal]) < err and state != 2:
+        q = np.array([pos.x, pos.y, theta])
+        #rospy.loginfo('U: '+str(U_grad))
+        q_goal = np.array([x_goal, y_goal, 0])
+        if np.linalg.norm(U_grad[1:2]) < err and state != 2:
             change_state(2)
-            rospy.loginfo('Changed to state 2: !! '+ states[state] +' !!')
         if state == 0:
-            v, w = motion2goal(x_goal, y_goal)
-            twist.linear.x = v
-            twist.angular.z = w
-            pub.publish(twist)
-        elif state == 1:
-            v, w = boundary_following(x_goal, y_goal)
+            U_att, grad_Uatt = attractive_potential(q, q_goal)
+            U_rep, grad_Urep = repulsive_potential(q)
+            U = U_att + U_rep
+            U_grad = grad_Uatt + grad_Urep
+            q = q - alpha * U_grad
+            v, w = traj_controller(q[0], q[1])
             twist.linear.x = v
             twist.angular.z = w
             pub.publish(twist)
 
-        #t.transform.translation = twist.linear
-        #t.transform.rotation = quaternion_from_euler(twist.angular.x, twist.angular.y, twist.angular.z)
         counter += 1
-        #rospy.loginfo(f'{x_goal}, {y_goal}')
-        #br.sendTransform(t)
         rate.sleep()
 
 
@@ -249,6 +268,5 @@ if __name__ == '__main__':
         x_goal = float(sys.argv[1])
         y_goal = float(sys.argv[2])
         run(x_goal, y_goal)
-        #rospy.spin()
     except rospy.ROSInterruptException:
         pass
